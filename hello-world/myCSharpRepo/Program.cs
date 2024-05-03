34a458143105c1140153d51c91d3050ea061b523
Swetlana2004 <167495638+Swetlana2004@users.noreply.github.com>
2024-05-03 09:47:40 +0700

    Add files via upload

---
+++ hello-world/myCSharpRepo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            using (StreamWriter writer = new StreamWriter("output.txt"))
+            {
+                writer.WriteLine("Hello world");
+            }
+        }
+    }
+}
